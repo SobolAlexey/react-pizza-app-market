98e7035fd44e641d3ba281a08cc919dc01cf759f
Aleksey <59054924+SobolAlexey@users.noreply.github.com>
2021-04-15 23:08:54 +0300

    fucking proxy

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -42,9 +42,7 @@ export const CartItem = ({id, name, type, size, totalPrice, totalCount, onRemove
             </div>
             <div className="cart__item-price">
                 <b>{totalPrice} ₽ </b>
-        <h></h>
-        <h></h>
-        <h></h>
+                <b>{totalPrice} ₽ </b>
             </div>
             <div className="cart__item-remove">
                 <Button onClick={handleRemoveClick} outline className="button--circle">
