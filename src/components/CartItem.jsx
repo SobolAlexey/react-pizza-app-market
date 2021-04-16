420d8543a58104117d063c881c84f8ed8b9dc219
Aleksey <59054924+SobolAlexey@users.noreply.github.com>
2021-04-16 06:15:58 +0300

    fucking server

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -42,7 +42,8 @@ export const CartItem = ({id, name, type, size, totalPrice, totalCount, onRemove
             </div>
             <div className="cart__item-price">
                 <b>{totalPrice} ₽ </b>
-                <b>{totalPrice} ₽ </b>
+                <b></b>
+                
             </div>
             <div className="cart__item-remove">
                 <Button onClick={handleRemoveClick} outline className="button--circle">
